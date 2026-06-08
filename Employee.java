3b05f71bf255484d40f6bea47c9e19e4ee3e4110
petrovartur <petrovartur989@gmail.com>
2026-06-08 21:38:44 +0500

    Employee.java

---
+++ Employee.java
@@ -0,0 +1,31 @@
+public class Employee {
+
+    private int id;
+    private String name;
+    private String role;
+
+    public Employee(int id, String name, String role) {
+        this.id = id;
+        this.name = name;
+        this.role = role;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    @Override
+    public String toString() {
+        return "ID: " + id +
+                ", Имя: " + name +
+                ", Роль: " + role;
+    }
+}
